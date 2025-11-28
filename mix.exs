2c69065a6b5c807a7a8656f2f1f269792660fca0
Willian Teophilo <wteophilo@gmail.com>
2025-11-28 15:32:02 -0300

    chore: add credo lint dependency

--- mix.exs
+++ mix.exs
@@ -66,7 +66,8 @@ defmodule Discuss.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:dotenvy, "~> 1.0.0"}
+      {:dotenvy, "~> 1.0.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
