41e3c3341c0c05c5eb22b4ccfcdf9dd4a0bb57b1
Willian Teophilo <wteophilo@gmail.com>
2025-11-28 15:22:21 -0300

    chore: add dotenvy dependency

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule Discuss.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:dotenvy, "~> 1.0.0"}
     ]
   end
 
